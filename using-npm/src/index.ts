5a8c83ea663735410aae4cf44b00c7f1ce2c0122
katp98 <kethzyp@gmail.com>
2022-09-21 09:10:20 +0530

    completed
    Signed-off-by: katp98 <kethzyp@gmail.com>

---
+++ using-npm/src/index.ts
@@ -0,0 +1,8 @@
+import logger from './helpers/logger';
+
+
+logger.info('Info level log message')
+logger.error('Error level log message')
+logger.debug('Debug level log message')
+logger.warn('Warning level log message')
+
